425b09d3ba2e0911915d70f55d79a8e32d53bcd5
Janeé Peña <janee.pena@gmail.com>
2020-03-29 21:22:08 -0600

    Adds retry if timout occurs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  post '/urbem' => 'webhooks#receive', as: :receive_webhooks
+  constraints format: :json do
+    post '/urbem' => 'webhooks#receive', as: :receive_webhooks
+  end
 
 end
--- spec/requests/webhooks_request_spec.rb
+++ spec/requests/webhooks_request_spec.rb
@@ -1,8 +1,20 @@
 require "rails_helper"
 
 feature "User creates citizen case", :js do
-  scenario "successfully" do
-    fake_urbem_webhook("citizen_case_created").send
+  scenario "receive hook successfully" do
+    fake_urbem_webhook("test_connection").send
+  end
+
+  describe "POST /urbem event", type: :request do
+    it "works correctly" do
+      post '/urbem', params: File.read(fixture_path)
+      expect(response.status).to eq(200)
+    end
+
+    it "do not send body" do
+      post '/urbem', params: nil
+      expect(response.status).not_to eq(200)
+    end
   end
 
   def fake_urbem_webhook(fixture)
@@ -12,5 +24,10 @@ feature "User creates citizen case", :js do
       path: "/urbem",
       port: Capybara.current_session.server.port,
     )
+  end
+
+  private
+  def fixture_path
+    "#{Rails.root}/spec/fixtures/webhooks/urbem/citizen_case_created.json"
   end
 end
